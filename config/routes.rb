9d496d681cae0a9fa48e063aeb635d56bdaf36af
Chris <yg012014@mymail.pomona.edu>
2016-08-03 08:05:54 -0700

    Install factory_girl gem, implement user signup, and deal with signup failures

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
